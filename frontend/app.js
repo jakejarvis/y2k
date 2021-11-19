8f55942b8c12205e8c71aff9f00caab82a4188f9
Jake Jarvis <jake@jarv.is>
2021-11-18 19:16:23 -0500

    bundle with esbuild

--- frontend/app.js
+++ frontend/app.js
@@ -1,10 +1,7 @@
-import RFB from "https://cdn.skypack.dev/pin/@novnc/novnc@v1.3.0-RRNSIy674lWHJVNg8Apd/mode=imports,min/unoptimized/core/rfb.js";
+import RFB from "@novnc/novnc/core/rfb.js";
 
 // DOS-style box for text
 const cmd = document.getElementById("cmd-text");
-
-// random tile wallpaper
-document.body.style.backgroundImage = "url('tiles/tile_" + Math.floor(20 * Math.random()) + ".jpg')";
 
 if (window.WebSocket) {
   // https://github.com/novnc/noVNC/blob/master/docs/API.md
